331c68347f42435b6257f4164e72411f2a6b4005
lucas picollo <lucaspicolloo@gmail.com>
2025-06-24 11:03:50 -0300

    chore: clear all packages

--- apps/contracts/script/Deploy.s.sol
+++ apps/contracts/script/Deploy.s.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.29 <0.9.0;
 
-import { Foo } from "../src/Foo.sol";
-
+import { GovToken } from "../src/GovToken.sol";
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/guides/scripting-with-solidity
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcast returns (GovToken token) {
+        token = new GovToken();
     }
 }
